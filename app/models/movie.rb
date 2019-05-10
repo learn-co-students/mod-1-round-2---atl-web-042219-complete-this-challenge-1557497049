2908b05a6e5c895fe8a9242e96a5c4eddc027429
Preston Elliott <preston.elliott24@gmail.com>
2019-05-10 10:58:16 -0400

    Completed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,21 @@ class Movie
     @@all
   end
 
+  def queue_items
+    QueueItem.all.select { |queue| queue.movie == self }
+  end
+
+  def viewers
+    queue_items.map { |queue| queue.viewer }
+  end
+
+  def average_rating
+    r_sum = queue_items.sum { |queue| queue.rating }
+    r_count = queue_items.count
+    r_avg = ((r_sum.to_f) / (r_count.to_f) ).to_f
+  end
+
+  def self.highest_rated
+    Movie.all.max_by { |movie| movie.average_rating}
+  end
 end
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,17 @@
 class QueueItem
+    attr_accessor :rating
+    attr_reader :viewer, :movie
 
+    @@queue = []
 
+    def initialize(viewer, movie, rating=nil)
+        @viewer = viewer 
+        @movie = movie
+        @rating = rating
+        @@queue << self
+    end
+
+    def self.all
+        @@queue
+    end
 end
